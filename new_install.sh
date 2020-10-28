4cbdfd7f414082ae9a03c71c2a6c22f155212185
saron67 <32464793+saron67@users.noreply.github.com>
2020-10-28 19:54:14 +0100

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -28,6 +28,16 @@ sudo dnf install -y darktable ffmpeg kdenlive digikam ImageMagick gimp blender i
 #vlc
 
 
+#VSCODE
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+sudo dnf check-update
+sudo dnf install code
+
+
+
+
 ##################################################################
 # bureautique
 ##################################################################
